59a5d49d5b91117a003be388e7e23160d183ed76
fledgless <cameriancardinal@gmail.com>
2025-04-18 14:43:12 +0200

    finished Eleve

--- Controllers/EleveController.js
+++ Controllers/EleveController.js
@@ -21,7 +21,43 @@ class EleveController {
         } catch (error) {
             console.log(error);
             res.status(500);
+            res.json({ error: 'Erreur lors de la récupération de l\'élève.' })
+        }
+    }
+    async addEleve(req, res) {
+        try {
+            const eleve = await EleveService.addEleve(req.body);
+            res.status(201).json(eleve);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
             res.json({ error: 'Erreur lors de l\'ajout de l\'élève.' })
+        }
+    }
+    async updateEleve(req, res) {
+        try {
+            const eleve = await EleveService.updateEleve(req.params.id, req.body);
+            if (!eleve) {
+                return res.status(404).json({ error: 'Eleve non trouvé.' });
+            }
+            res.json(eleve);
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+            res.json({ error: 'Erreur lors de la mise à jour de l\'élève.' })
+        }
+    }
+    async deleteEleve(req, res) {
+        try {
+            const eleve = await EleveService.deleteEleve(req.params.id);
+            if (!eleve) {
+                return res.status(404).json({ error: 'Eleve non trouvé.' });
+            }
+            res.json({ message: 'Elève supprimé avec succès.' })
+        } catch (error) {
+            console.log(error);
+            res.status(500);
+            res.json({ error: 'Erreur lors de la mise à jour de l\'élève.' })
         }
     }
 }
--- Routes/EleveRoute.js
+++ Routes/EleveRoute.js
@@ -5,5 +5,17 @@ const EleveController = require('../Controllers/EleveController');
 router.get("/", (req, res) => {
     EleveController.getAllEleves(req, res);
 })
+router.get("/:id", (req, res) => {
+    EleveController.getEleveById(req, res);
+})
+router.post("/", (req, res) => {
+    EleveController.addEleve(req, res);
+})
+router.put("/:id", (req, res) => {
+    EleveController.updateEleve(req, res);
+})
+router.delete("/:id", (req, res) => {
+    EleveController.deleteEleve(req, res);
+})
 
 module.exports = router;
